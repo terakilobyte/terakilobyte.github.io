8ecc7011de0f64484983db8e37cfcd6cf22f568b
terakilobyte <terakilobyte@gmail.com>
2015-12-06 07:22:06 -0800

    Add tictactoe action definitions

---
+++ src/actions/tictactoe.js
@@ -0,0 +1,15 @@
+import { PLAYER_MOVE,
+         COMPUTER_MOVE,
+         PLAYER_WIN,
+         COMPUTER_WIN,
+         DRAW_GAME,
+         DEPTH_INCREMENT } from 'constants/tictactoe';
+
+export default {
+  player_move: () => ({ type : PLAYER_MOVE }),
+  computer_move: () => ({ type : COMPUTER_MOVE }),
+  player_win: () => ({ type : PLAYER_WIN }),
+  computer_win: () => ({ type : COMPUTER_WIN }),
+  draw_game: () => ({ type : DRAW_GAME }),
+  depth_increment: () => ({ type : DEPTH_INCREMENT })
+};
---
+++ src/constants/tictactoe.js
@@ -0,0 +1,10 @@
+import { createConstants } from '../utils';
+
+export default createConstants(
+  'PLAYER_MOVE',
+  'COMPUTER_MOVE',
+  'PLAYER_WIN',
+  'COMPUTER_WIN',
+  'DRAW_GAME',
+  'DEPTH_INCREMENT'
+);
