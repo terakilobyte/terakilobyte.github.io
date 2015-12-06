e5eb07db08847eb617795b46adaa00405a2b0254
terakilobyte <terakilobyte@gmail.com>
2015-12-05 21:09:11 -0800

    Update playgroundview

--- src/views/PlaygroundView.js
+++ src/views/PlaygroundView.js
@@ -5,7 +5,6 @@ import ProjectWell from 'components/ProjectWell/ProjectWell';
 const PlaygroundView = () => {
   return (
     <div>
-      <Link to='/'>Back</Link>
       <ProjectWell link='https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Tic_tac_toe.svg/2000px-Tic_tac_toe.svg.png'
         source='https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Tic_tac_toe.svg/2000px-Tic_tac_toe.svg.png'
       />
