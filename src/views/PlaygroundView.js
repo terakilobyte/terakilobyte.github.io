6ea84abf6f3185335644ce2fbf88678b34e9bf26
Nathan <terakilobyte@gmail.com>
2015-12-13 09:13:46 -0800

    Remove duplicate tictactoe playgrounds

--- src/views/PlaygroundView.js
+++ src/views/PlaygroundView.js
@@ -6,26 +6,6 @@ const projects = [
   {
     link: '/tictactoe',
     source: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Tic_tac_toe.svg/2000px-Tic_tac_toe.svg.png'
-  },
-  {
-    link: '/tictactoe',
-    source: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Tic_tac_toe.svg/2000px-Tic_tac_toe.svg.png'
-  },
-  {
-    link: '/tictactoe',
-    source: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Tic_tac_toe.svg/2000px-Tic_tac_toe.svg.png'
-  },
-  {
-    link: '/tictactoe',
-    source: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Tic_tac_toe.svg/2000px-Tic_tac_toe.svg.png'
-  },
-  {
-    link: '/tictactoe',
-    source: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Tic_tac_toe.svg/2000px-Tic_tac_toe.svg.png'
-  },
-  {
-    link: '/tictactoe',
-    source: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Tic_tac_toe.svg/2000px-Tic_tac_toe.svg.png'
   }
 ];
 
