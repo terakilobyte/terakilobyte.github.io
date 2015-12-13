009e3a12d21aa5b04acd1f17616a6af2458046b1
terakilobyte <terakilobyte@gmail.com>
2015-12-12 17:36:38 -0800

    Add console statements for initial notification

--- src/projects/TicTacToe/TicTacToe.js
+++ src/projects/TicTacToe/TicTacToe.js
@@ -7,7 +7,8 @@ import GameBoard from './components/GameBoard';
 
 const mapStateToProps = (state) => {
   return {
-    playerTurn: state.tictactoe.playerTurn
+    playerTurn: state.tictactoe.playerTurn,
+    winner: state.tictactoe.winner
   };
 };
 
@@ -24,8 +25,10 @@ class TicTacToe extends React.Component {
     }
     if (propObj.winner && propObj.winner.result === 'draw') {
       // do very cool modal fadein
+      console.log('DRAW');
     } else if (propObj.winner) {
       // do very cool modal fadein with computer winning
+      console.log('YOU LOST');
     }
     // nothing else, the player can't win
   }
@@ -34,6 +37,8 @@ class TicTacToe extends React.Component {
     return (
       <div>
         <h1 className='text-center'>Tic-Tac-Toe</h1>
+        <h2 className='text-center'>Open your console to see messages</h2>
+        <h3 className='text-center'>You can't win</h3>
         <GameBoard />
       </div>
     );
